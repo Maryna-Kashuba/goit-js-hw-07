3c1662c2087dac356516d3bfa4652a5195261bc2
Maryna-Kashuba <144819587+Maryna-Kashuba@users.noreply.github.com>
2024-01-06 22:03:11 +0200

    task 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,32 @@
+
+const categoriesList = document.getElementById('categories');
+
+const categoryCount = categoriesList.children.length;
+
+console.log(`Number of categories: ${categoryCount}`);
+
+categoriesList.querySelectorAll('.item').forEach(category => {
+    const categoryName = category.querySelector("h2").textContent;
+    const itemCount = category.querySelectorAll("li").length;
+
+    console.log(`Category: ${categoryName}`);
+    console.log(`Elements: ${itemCount}`);
+});
+
+
 // З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
 
-// Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
-// Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
+// 1)Порахує й виведе в консоль кількість категорій в ul#categories, 
+// тобто елементів li.item.
+//2) Для кожного елемента li.item у списку ul#categories знайде й 
+//виведе в консоль текст заголовка елемента(тегу < h2 >) і
+// кількість елементів у категорії(усіх < li >, вкладених у нього).
+
+
+
 // На що буде звертати увагу ментор при перевірці:
-// Кількість категорій, їх назва та кількість елементів отримані за допомогою властивостей і методів DOM-елементів
-// Дані за кожною категорією були отримані й виведені в консоль у тілі циклу або методу forEach()
+// Кількість категорій, їх назва та кількість елементів отримані
+// за допомогою властивостей і методів DOM - елементів
+// Дані за кожною категорією були отримані й виведені в консоль у 
+//тілі циклу або методу forEach()
 // У консолі має бути виведено наступне повідомлення:
