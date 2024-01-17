0c1c9178f540079c046a150a3f8708bb3159ad06
Maryna-Kashuba <144819587+Maryna-Kashuba@users.noreply.github.com>
2024-01-17 12:36:57 +0200

    task3

--- js/task-1.js
+++ js/task-1.js
@@ -1,32 +1,32 @@
 
-const categoriesList = document.getElementById('categories');
+// const categoriesList = document.getElementById('categories');
 
-const categoryCount = categoriesList.children.length;
+// const categoryCount = categoriesList.children.length;
 
-console.log(`Number of categories: ${categoryCount}`);
+// console.log(`Number of categories: ${categoryCount}`);
 
-categoriesList.querySelectorAll('.item').forEach(category => {
-    const categoryName = category.querySelector("h2").textContent;
-    const itemCount = category.querySelectorAll("li").length;
+// categoriesList.querySelectorAll('.item').forEach(category => {
+//     const categoryName = category.querySelector("h2").textContent;
+//     const itemCount = category.querySelectorAll("li").length;
 
-    console.log(`Category: ${categoryName}`);
-    console.log(`Elements: ${itemCount}`);
-});
-
-
-// З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
-
-// 1)Порахує й виведе в консоль кількість категорій в ul#categories, 
-// тобто елементів li.item.
-//2) Для кожного елемента li.item у списку ul#categories знайде й 
-//виведе в консоль текст заголовка елемента(тегу < h2 >) і
-// кількість елементів у категорії(усіх < li >, вкладених у нього).
+//     console.log(`Category: ${categoryName}`);
+//     console.log(`Elements: ${itemCount}`);
+// });
 
 
+// // З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
 
-// На що буде звертати увагу ментор при перевірці:
-// Кількість категорій, їх назва та кількість елементів отримані
-// за допомогою властивостей і методів DOM - елементів
-// Дані за кожною категорією були отримані й виведені в консоль у 
-//тілі циклу або методу forEach()
-// У консолі має бути виведено наступне повідомлення:
+// // 1)Порахує й виведе в консоль кількість категорій в ul#categories, 
+// // тобто елементів li.item.
+// //2) Для кожного елемента li.item у списку ul#categories знайде й 
+// //виведе в консоль текст заголовка елемента(тегу < h2 >) і
+// // кількість елементів у категорії(усіх < li >, вкладених у нього).
+
+
+
+// // На що буде звертати увагу ментор при перевірці:
+// // Кількість категорій, їх назва та кількість елементів отримані
+// // за допомогою властивостей і методів DOM - елементів
+// // Дані за кожною категорією були отримані й виведені в консоль у 
+// //тілі циклу або методу forEach()
+// // У консолі має бути виведено наступне повідомлення:
--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,12 @@
-// Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input) підставляє його поточне значення в span#name-output як ім’я для привітання. Обов’язково очищай значення в інпуті по краях від пробілів . Якщо інпут порожній або містить лише пробіли, то замість імені у спан має підставлятися рядок "Anonymous".
+const nameInput = document.getElementById('name-input');
+const nameOutput = document.getElementById('name-output');
 
-// <input type="text" id="name-input" placeholder="Please enter your name" />
-// <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
+nameInput.addEventListener('input', (newNameOutput) => {
+    nameOutput.textContent = newNameOutput.currentTarget.value;
+    const trimmedValue = nameInput.value.trim();
+  nameOutput.textContent = trimmedValue === '' ? 'Anonymous' : trimmedValue;
+});
 
-// На що буде звертати увагу ментор при перевірці:
-// На елементі input#name-input прослуховується подія input
-// Під час набору тексту в інпуті його поточне значення підставляється в span#name-output як ім’я для привітання
-// Значення в інпуті очищене від пробілів по краях
-// Якщо інпут порожній або містить лише пробіли, то замість імені у спан має підставлятися рядок "Anonymous"
+
+
+
